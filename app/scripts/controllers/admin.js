aecb38eb865ab369ecc0c9d1d3ac18e8e0702d31
FelixLC <felix.lechevallier.com>
2013-12-18 04:52:01 +0100

    adminviews

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -22,7 +22,14 @@ angular.module('vinifyApp')
 					});
 
 				$scope.A = [];
+				$scope.B = [];
+				$scope.C = [];
+				$scope.D = [];
+				$scope.actionlist = ['PostVinibar', 'PostRefill', 'PostBottle', 'PopulateWines'];
 				$scope.winenumber = ['wine1', 'wine2', 'wine3', 'wine4', 'wine5', 'wine6', 'wine7', 'wine8', 'wine9'];
+				$scope.winerefillnumber = ['wine1', 'wine2', 'wine3'];
+				$scope.winebottlenumber = ['wine1'];
+				$scope.temps = ['6 - 8', '7 - 10', '10 - 13', '13', '15 - 18'];
 
 				$scope.PostWines = function(){
 					var dataset = 'username=' + $scope.A[0]
@@ -46,4 +53,57 @@ angular.module('vinifyApp')
 												});
 				return request;
 				};
+
+				$scope.PostRefill = function(){
+					var dataset = 'username=' + $scope.B[0]
+											+ '&wine1=' + $scope.B[1]
+											+ '&wine2=' + $scope.B[2]
+											+ '&wine3=' + $scope.B[3];
+
+					var request = $http({
+													url: '/createrefill/',
+													method: 'POST',
+													data: dataset,
+													headers: {
+														'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+													}
+												});
+				return request;
+				};
+
+				$scope.PostBottle = function(){
+					var dataset = 'username=' + $scope.C[0]
+											+ '&wine1=' + $scope.C[1];
+
+					var request = $http({
+													url: '/createbottle/',
+													method: 'POST',
+													data: dataset,
+													headers: {
+														'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+													}
+												});
+				return request;
+				};
+
+				$scope.PopulateWines = function(){
+					var dataset = 'wine=' + $scope.D[0]
+											+ '&description=' + $scope.D[1]
+											+ '&degustation=' + $scope.D[2]
+											+ '&temp=' + $scope.D[3]
+											+ '&caraf=' + $scope.D[4];
+
+					var request = $http({
+													url: '/populatewines/',
+													method: 'POST',
+													data: dataset,
+													headers: {
+														'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
+													}
+												});
+				return request;
+				};
+
+				
+
 			});
