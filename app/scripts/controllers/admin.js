72d9263b83fb6f56b4af9e9a94253c7fd6fced0f
FelixLC <felix.lechevallier.com>
2014-01-08 13:56:46 +0100

    added °c

--- app/scripts/controllers/admin.js
+++ app/scripts/controllers/admin.js
@@ -29,29 +29,17 @@ angular.module('vinifyApp')
 					function(error){
 						console.log(error);
 					});
-				VinibarUsers.getWineRatings(
-					// Success
-					function(cda){
-						$scope.wineratings=cda;
-					},
-					// Failure
-					function(error){
-						console.log(error);
-					});
-				VinibarUsers.getUserRatings(
-					// Success
-					function(cda){
-						$scope.userratings=cda;
-					},
-					// Failure
-					function(error){
-						console.log(error);
-					});
 
+				$scope.theuser;
+				$scope.thewine;
 				$scope.A = [];
 				$scope.B = [];
 				$scope.C = [];
 				$scope.D = [];
+				$scope.E = [];
+				$scope.F = [];
+				$scope.G = [];
+				$scope.H = [];
 				$scope.actionlist = ['PostVinibar', 'PostRefill', 'PostBottle', 'PopulateWines', 'ClientsOverview', 'WineRatings', 'UserRatings'];
 				$scope.winenumber = ['wine1', 'wine2', 'wine3', 'wine4', 'wine5', 'wine6', 'wine7', 'wine8', 'wine9'];
 				$scope.winerefillnumber = ['wine1', 'wine2', 'wine3'];
@@ -132,7 +120,7 @@ angular.module('vinifyApp')
 				};
 
 				$scope.GetUserRatings = function(){
-					var dataset = 'username=' + $scope.theuser;
+					var dataset = 'username=' + $scope.E[0];
 
 					var request = $http({
 													url: '/userratings/',
@@ -142,11 +130,14 @@ angular.module('vinifyApp')
 														'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 													}
 												});
-				return request;
+				return request
+					.success(function(data, status, headers, config) {
+						$scope.G = data;
+					});
 				};
 
 				$scope.GetWineRatings = function(){
-					var dataset = 'wine=' + $scope.thewine;
+					var dataset = 'wine=' + $scope.F[0];
 
 					var request = $http({
 													url: '/wineratings/',
@@ -156,13 +147,11 @@ angular.module('vinifyApp')
 														'Content-Type': 'application/x-www-form-urlencoded; charset=UTF-8'
 													}
 												});
-				return request;
+				return request
+					.success(function(data, status, headers, config) {
+						$scope.H = data;
+					});
 				};
 
 				
-
-			//TableController
-				$scope.clientoverview = [{"rating_count": 0, "bottle_count": 9, "username": "marchandise@yahoo.fr", "id": 32}, {"rating_count": 0, "bottle_count": 9, "username": "matthieu.cord@gmail.com", "id": 33}, {"rating_count": 0, "bottle_count": 9, "username": "gatien.bon@polytechnique.edu", "id": 34}, {"rating_count": 0, "bottle_count": 9, "username": "eschocron@gmail.com", "id": 35}, {"rating_count": 0, "bottle_count": 9, "username": "rubib@hotmail.fr", "id": 36}, {"rating_count": 0, "bottle_count": 9, "username": "fbertozzi@aneo.fr", "id": 37}, {"rating_count": 5, "bottle_count": 4, "username": "felix@vinify.co", "id": 2}, {"rating_count": 0, "bottle_count": 0, "username": "samuel@sampaccoud.com", "id": 38}, {"rating_count": 0, "bottle_count": 0, "username": "mathieu2424@hotmail.fr", "id": 39}, {"rating_count": 0, "bottle_count": 0, "username": "ppdebouville@gmail.com", "id": 40}, {"rating_count": 0, "bottle_count": 0, "username": "underbararesa@gmail.com", "id": 41}, {"rating_count": 0, "bottle_count": 9, "username": "annevb@hotmail.fr", "id": 11}, {"rating_count": 0, "bottle_count": 0, "username": "mabouquin@gmail.com", "id": 5}, {"rating_count": 0, "bottle_count": 0, "username": "emmanuel.legrand1@sfr.fr", "id": 6}, {"rating_count": 0, "bottle_count": 9, "username": "camuset.courcelles@orange.fr", "id": 7}, {"rating_count": 0, "bottle_count": 0, "username": "pierreedouardvidal@gmail.com", "id": 8}, {"rating_count": 0, "bottle_count": 0, "username": "scotty_schneider@yahoo.fr", "id": 9}, {"rating_count": 0, "bottle_count": 0, "username": "sylvie.gerard1000@free.fr", "id": 10}, {"rating_count": 0, "bottle_count": 0, "username": "jcb.mathieu@gmail.com", "id": 13}, {"rating_count": 0, "bottle_count": 0, "username": "frroyer@hotmail.com", "id": 14}, {"rating_count": 0, "bottle_count": 0, "username": "arthur.auxenfants@hotmail.fr", "id": 15}, {"rating_count": 0, "bottle_count": 0, "username": "gviennet@aastra.com", "id": 17}, {"rating_count": 0, "bottle_count": 9, "username": "aurelien.cord@gmail.com", "id": 18}, {"rating_count": 0, "bottle_count": 0, "username": "guillaume.debaudreuil@gmail.com", "id": 19}, {"rating_count": 0, "bottle_count": 0, "username": "marksoubiran@hotmail.com", "id": 20}, {"rating_count": 0, "bottle_count": 0, "username": "patlyproductions@hotmail.com", "id": 21}, {"rating_count": 0, "bottle_count": 9, "username": "nplanty@gmail.com", "id": 22}, {"rating_count": 0, "bottle_count": 0, "username": "dlechevallier@verspieren.com", "id": 23}, {"rating_count": 0, "bottle_count": 0, "username": "quentin.legoc@edhec.com", "id": 26}, {"rating_count": 0, "bottle_count": 9, "username": "tdewaziers@gmail.com", "id": 27}, {"rating_count": 0, "bottle_count": 0, "username": "potel.laurent@gmail.com", "id": 28}, {"rating_count": 0, "bottle_count": 9, "username": "pierre.ichoua@gmail.com", "id": 29}, {"rating_count": 0, "bottle_count": 0, "username": "remy.fabrice@neuf.fr", "id": 30}, {"rating_count": 0, "bottle_count": 0, "username": "sandrine.schulz@gmail.com", "id": 31}, {"rating_count": 0, "bottle_count": 0, "username": "gillesmeese@gmail.com", "id": 25}, {"rating_count": 0, "bottle_count": 9, "username": "yves.lajouanie@gmail.com", "id": 12}, {"rating_count": 9, "bottle_count": 9, "username": "benjamin@vinify.co", "id": 1}, {"rating_count": 0, "bottle_count": 9, "username": "philippeplaton@gmail.com", "id": 24}, {"rating_count": 0, "bottle_count": 0, "username": "antoine@vinify.co", "id": 3}, {"rating_count": 1, "bottle_count": 8, "username": "michael@vinify.co", "id": 4}, {"rating_count": 0, "bottle_count": 9, "username": "laurentcadoret@free.fr", "id": 16}];
-
-
 			});
